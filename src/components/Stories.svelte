9ca83d948bc6ea1dfb313d36e1ea9b8416c008d3
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2020-06-04 21:26:17 -0600

    added styles

--- src/components/Stories.svelte
+++ src/components/Stories.svelte
@@ -1,9 +1,63 @@
 <script>
     
 </script>
-
 <style>
-    /* your styles go here */
+  .Stories {
+    border: 1px solid rgba(219, 219, 219, 1);
+    border-radius: 4px;
+    margin: 1em 0;
+    padding: 0.5em 1em;
+    background-color: white;
+  }
+  .Stories-item img {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+  }
+  .Stories-item {
+    display: flex;
+    align-items: center;
+    margin: 0 0 0.5em 0;
+  }
+  .Stories-item h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0 0 0 0.5em;
+  }
+  .Stories-item h2 span {
+    display: block;
+    font-size: 9px;
+    text-transform: uppercase;
+    color: gray;
+  }
+  .Stories-head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5em 0 0 0;
+  }
+  .Stories-head h2 {
+    font-size: 12px;
+    font-weight: 600;
+  }
+  .Stories-head span {
+    font-size: 12px;
+    font-weight: normal;
+    color: black;
+  }
+  .Stories-items {
+    margin: 0.5em 0 0.5em 0;
+  }
+  .Stories-item-box {
+    width: 32px;
+    height: 32px;
+    border: double 2px transparent;
+    border-radius: 100%;
+    background-image: linear-gradient(#fff, #fff),
+      radial-gradient(circle at top left, #f09433, #bc1888);
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+  }
 </style>
 
 <!-- markup (zero or more items) goes here -->
@@ -16,9 +70,9 @@
         <div class="Stories-items">
             <div class="Stories-item">
                 <div class="Stories-item-box">
-                    <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="">
-                    <h2>byariel <span>10 horas antes</span></h2>
+                    <img src="https://scontent.fgua3-1.fna.fbcdn.net/v/t1.0-9/67344453_2374585109277194_8601813831590608896_o.jpg?_nc_cat=101&_nc_sid=09cbfe&_nc_ohc=0c1QW3vipG4AX80gSHs&_nc_ht=scontent.fgua3-1.fna&oh=9dc748b6bcc58cb558ef28b11c1e7d52&oe=5EF4DB94" alt="">
                 </div>
+                <h2>byariel<span>10 horas antes</span></h2>
             </div>
         </div>
     </div>
