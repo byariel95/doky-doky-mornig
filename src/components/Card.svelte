390b1d91e20c859d6f47a64e7c2a0f7c2106ab3d
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2020-06-18 20:27:23 -0600

    added share modal

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,5 +1,9 @@
 <script>
 import Comments from './Comments.svelte';
+import Modal from './Modal.svelte';
+import Share from './Share.svelte';
+import  { blur } from 'svelte/transition';
+
 
 export let username;
 export let location;
@@ -7,6 +11,11 @@ export let photo;
 export let postComment;
 export let comments;
 export let avatar;
+
+let isModal = false;
+function handleClick() {
+  isModal = !isModal;
+}
 
 </script>
 
@@ -117,6 +126,15 @@ export let avatar;
 
 
 <div class="Card">
+
+    {#if isModal}
+      <div transition:blur>
+          <Modal>
+            <Share on:click={handleClick}/>
+          </Modal>
+      </div>
+    {/if}
+
     <div class="Card-container">
         <div class="Card-header">
             <div class="Card-user">
@@ -138,7 +156,7 @@ export let avatar;
         <div class="Card-icons">
             <div class="Card-icons-firts">
                 <i class="fas fa-heart" />
-                <i class="fas fa-paper-plane" />
+                <i class="fas fa-paper-plane" on:click={handleClick} />
             </div>
             <div class="Card-icons-second">
                 <i class="fas fa-bookmark" />
